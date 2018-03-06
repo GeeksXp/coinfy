ccf298c1e66afc1a54cbbf5cf4eb949d09bd728c
Yevhen Leontiev <eleontyev@geeksforless.net>
2018-03-06 14:04:25 +0200

    add data of user

---
+++ src/const/users.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    login: 'admin',
+    name: 'Clark',
+    password: 'MTIzNDU=' //12345
+  },
+  {
+    login: 'user',
+    name: 'Mark',
+    password: 'MTIzNDU=' //12345 
+  },
+]
+
+export default users;
