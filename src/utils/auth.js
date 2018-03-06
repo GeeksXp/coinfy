a1eb962ca8bd61cffdaee635c7262b7dd673924d
Yevhen Leontiev <eleontyev@geeksforless.net>
2018-03-06 14:36:39 +0200

    added model of auth

---
+++ src/utils/auth.js
@@ -0,0 +1,27 @@
+import {localStorageSet, localStorageGet, localStorageRemove} from '/api/browser'
+import state from '/store/state'
+import {setHref} from '/store/actions'
+import routes from '/router/routes'
+
+const KEY = 'profile'
+
+export function signIn(value) {
+  localStorageSet(KEY, value)
+  state.isLoggedIn = true
+  setHref(routes.home())
+}
+
+export function signOut() {
+  localStorageRemove(KEY)
+  state.isLoggedIn = false
+  setHref(routes.signIn())
+}
+
+export function isAuth() { 
+  if(localStorageGet(KEY)) {
+    state.isLoggedIn = true
+  } else {
+    state.isLoggedIn = false
+    setHref(routes.signIn())
+  }
+}
