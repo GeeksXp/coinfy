19ac30a7fa92a11f5b331fc79e354138e0dfe58d
Yevhen Leontiev <eleontyev@geeksforless.net>
2018-03-09 17:06:11 +0200

    added welcome message

--- src/components/views/SignIn.js
+++ src/components/views/SignIn.js
@@ -50,6 +50,7 @@ export default class SignIn extends Component {
 				let dataProfile = Object.assign({}, result[0])
 				delete dataProfile.password
 				signIn(JSON.stringify(dataProfile))
+				addNotification(`Welcome ${dataProfile.login}`)
 			} else {
 				addNotification(`Invalid login or password. Please try again.`, ERROR)
 			}
